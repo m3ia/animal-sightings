2815ec3d12d9e1ba5a0215ae6c85f32e3053cd05
m3ia <meianatividad@gmail.com>
2022-09-26 21:05:51 -0700

    GET all sightings

--- server/server.mjs
+++ server/server.mjs
@@ -1,13 +1,32 @@
 import express from 'express';
 import cors from 'cors';
+import bodyParser from 'body-parser';
+import pgPromise from 'pg-Promise';
+
 
 const app = express();
 const PORT = 8080;
 
 app.use(cors());
+app.use(bodyParser.json());
 app.listen(PORT, () => {
   console.log(`Hola this server is running on port ${PORT}`);
 })
+
+// DB connection
+const pgp = pgPromise({});
+const db = pgp('postgres://localhost:5432/sightings');
+
 app.get('/', (req, res) => {
   res.json('Hiyeee');
 })
+
+// GET - All Sightings --------------------------------------------------------
+app.get('/sightings', async function (req, res) {
+  try {
+    const sightings = await db.any('SELECT * FROM sightings ORDER BY id');
+    res.send(sightings);
+  } catch (e) {
+    return res.status(400).json({ e });
+  }
+});
